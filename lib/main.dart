82b25d832a6b696d5187ab311c82b93ecf90793b
WardlyCode <ward.justinlee@gmail.com>
2020-08-15 18:02:41 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: ScrumPoker(),
+    ),
+  );
+}
+
+class ScrumPoker extends StatefulWidget {
+  @override
+  _ScrumPokerState createState() => _ScrumPokerState();
+}
+
+class _ScrumPokerState extends State<ScrumPoker> {
+  int _currentIndex = 0;
+
+  final tabs = [
+    Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Card(
+            color: Colors.white,
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Session ID',
+              ),
+            ),
+          ),
+        ],
+      ),
+    ),
+    Center(
+      child: Text('Search'),
+    ),
+    Center(
+      child: Text('Profile'),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.black,
+        backgroundColor: Colors.white,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            title: Text('Session'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text('Search'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Profile'),
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
