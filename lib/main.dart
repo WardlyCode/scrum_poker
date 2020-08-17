4e91f95f72ebb7c3733d2facf48049ac12cedd80
WardlyCode <ward.justinlee@gmail.com>
2020-08-16 18:23:51 -0700

    join/create page, form fields and floating buttons

--- lib/main.dart
+++ lib/main.dart
@@ -17,20 +17,99 @@ class _ScrumPokerState extends State<ScrumPoker> {
   int _currentIndex = 0;
 
   final tabs = [
-    Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Card(
-            color: Colors.white,
-            child: TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Session ID',
+    Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Center(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            width: 350,
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: new BorderRadius.all(
+                new Radius.circular(20),
               ),
             ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Join a room'),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Session ID',
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    FloatingActionButton.extended(
+                      onPressed: () {
+                        print('Room Joined');
+                      },
+                      label: Text('Join'),
+                      backgroundColor: Colors.pink[400],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          width: 350,
+          height: 250,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: new BorderRadius.all(
+              new Radius.circular(20),
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('Create a room'),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Session ID',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Password (Optional)',
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FloatingActionButton.extended(
+                    onPressed: () {
+                      print('Room created');
+                    },
+                    label: Text('Create'),
+                    backgroundColor: Colors.blue[400],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     ),
     Center(
       child: Text('Search'),
